452979f45d6396d23aec1630c521a907a436188a
Love4653 <league@iMac.attlocal.net>
2017-01-29 17:23:07 -0800

    gg
    gg

--- src/Block.java
+++ src/Block.java
@@ -2,60 +2,67 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Block extends GameObject2 {
-int speed;
-static boolean upkey;
-static boolean downkey;
-static boolean rightkey;
-static boolean leftkey;
-int velocity;
-int gravity;
-	public static void main(String[] args) {
-	
+	int speed;
+	static boolean upkey;
+	static boolean downkey;
+	static boolean rightkey;
+	static boolean leftkey;
+	static boolean space;
+	boolean canJump = true;
+	int velocity;
+	int gravity;
+	int jumpPower;
+	int ground;
+
+	Block(int x, int y) {
+		super();
+		jumpPower = 20;
+		this.x = x;
+		width = 35;
+		height = 35;
+		this.y = y;
+		speed = 10;
+		upkey = false;
+		downkey = false;
+		rightkey = false;
+		leftkey = false;
+		gravity = 1;
+		velocity = 0;
+		ground = 725;
+	}
+
+	void update() {
+
+		if (rightkey) {
+			x++;
+		}
+		if (leftkey) {
+			x--;
+		}
+		velocity += gravity;
+		y += velocity;
+		if (y > ground) {
+			y = ground;
+			canJump = true;
+			velocity = 0;
+		} else {
+			canJump = false;
+		}
 
 	}
-Block(int x, int y){
-	super();
-	this.x=x;
-width = 35;
-height = 35;
-this.y = y;
-speed = 2;
-upkey = false;
-downkey = false;
-rightkey = false;
-leftkey = false;
-gravity = 1;
-velocity = 0;
-}
-void update(){
 
-	if(upkey){
-	y--;
-	
-}
-	
-if(downkey){
-	y++;
-}
-if(rightkey){
-	x++;
-}
-if(leftkey){
-	x--;
-}
-if(y >= 750){
-	y = 750;
-}
-else{
-	velocity+=gravity;
-	y+=velocity;
-}
-}
-void draw(Graphics z){
-	z.setColor(Color.BLUE);
-	z.fillRect(x, y, width, height);
-	super.draw(z);
-	
-	
-}
+	void draw(Graphics z) {
+		z.setColor(Color.BLUE);
+		z.fillRect(x, y, width, height);
+		super.draw(z);
+
+	}
+
+	void jump() {
+		if (canJump) {
+
+			velocity -= jumpPower;
+			System.out.println(velocity);
+		}
+	}
 }
--- src/GamePanel2.java
+++ src/GamePanel2.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,98 +16,119 @@ public class GamePanel2 extends JPanel implements ActionListener, KeyListener {
 	final int END_STATE = 2;
 	int currentState = MENU_STATE;
 	Block Blocky;
-GamePanel2(){
-	timer = new Timer(1000/60, this);	
-	 Blocky = new Block(50, 50);
+	Font font;
+
+	GamePanel2() {
+		timer = new Timer(1000 / 60, this);
+		Blocky = new Block(50, 50);
 		Blocky.x = 250;
 		Blocky.y = 400;
+		font = new Font("Arial", Font.PLAIN, 48);
 	}
 
-@Override
-public void actionPerformed(ActionEvent e) {
-	if(currentState == MENU_STATE){
-		updateMenuState( );
-	}else if(currentState == GAME_STATE){
-		updateGameState( );
-	}else if(currentState == END_STATE){
-		updateEndState();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (currentState == MENU_STATE) {
+			updateMenuState();
+		} else if (currentState == GAME_STATE) {
+			updateGameState();
+		} else if (currentState == END_STATE) {
+			updateEndState();
+		}
+		repaint();
 	}
-	repaint();
-}
-void startGame(){
-	timer.start();
-}
-public void paintComponent(Graphics z){
-	if(currentState == MENU_STATE){
-		drawMenuState(z);
-	}else if(currentState == GAME_STATE){
-		drawGameState(z);
-	}else if(currentState == END_STATE){
-		drawEndState(z);
-	}
-	repaint();
-}
-void updateGameState(){
-	Blocky.update();
-	
-}
-void updateMenuState(){
-	
-}
-void updateEndState(){
-	
-}
-void drawMenuState(Graphics z){
-	z.setColor(Color.RED);
-	z.fillRect(0, 0, Jump.width, Jump.height);  
-	//z.setFont(); 
-	z.setColor(Color.BLUE);
-	z.drawString("JUMP", 500, 100);
-}
-void drawGameState(Graphics z){
-	z.setColor(Color.BLACK);
-	z.fillRect(0, 0, Jump.width, Jump.height);  
-	//z.setFont(); 
-	Blocky.draw(z);
 
-}
-void drawEndState(Graphics z){
-	z.setColor(Color.LIGHT_GRAY);
-	z.fillRect(0, 0, Jump.width, Jump.height);  
-	//z.setFont(); 
-	
-}
-@Override
-public void keyTyped(KeyEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+	void startGame() {
+		timer.start();
+	}
 
-@Override
-public void keyPressed(KeyEvent e) {
-	if(e.getKeyCode()==KeyEvent.VK_ENTER)
-	{
-		currentState++;
-		
-		if(currentState > END_STATE){
-			currentState = MENU_STATE;
-		}	
-	}	
-	if(e.getKeyCode()==KeyEvent.VK_RIGHT){
-		Blocky.rightkey = true;
+	public void paintComponent(Graphics z) {
+		if (currentState == MENU_STATE) {
+			drawMenuState(z);
+		} else if (currentState == GAME_STATE) {
+			drawGameState(z);
+		} else if (currentState == END_STATE) {
+			drawEndState(z);
+		}
+		repaint();
 	}
-	if(e.getKeyCode()==KeyEvent.VK_LEFT){
-		Blocky.leftkey = true;
-	}
-}
 
-@Override
-public void keyReleased(KeyEvent e) {
-	if(e.getKeyCode()==KeyEvent.VK_RIGHT){
-		Blocky.rightkey = false;
-	}	
-	if(e.getKeyCode()==KeyEvent.VK_LEFT){
-		Blocky.leftkey = false;
+	void updateGameState() {
+		Blocky.update();
+
 	}
-}
+
+	void updateMenuState() {
+
+	}
+
+	void updateEndState() {
+
+	}
+
+	void drawMenuState(Graphics z) {
+		z.setColor(Color.RED);
+		z.fillRect(0, 0, Jump.width, Jump.height);
+		z.setFont(font);
+		z.setColor(Color.BLUE);
+		z.drawString("JUMP", 400, 100);
+		z.setColor(Color.BLUE);
+		z.drawString("Press Enter To Start", 250, 500);
+	}
+
+	void drawGameState(Graphics z) {
+		z.setColor(Color.BLACK);
+		z.fillRect(0, 0, Jump.width, Jump.height);
+		// z.setFont();
+		Blocky.draw(z);
+
+	}
+
+	void drawEndState(Graphics z) {
+		z.setColor(Color.LIGHT_GRAY);
+		z.fillRect(0, 0, Jump.width, Jump.height);
+		z.setFont(font);
+		z.drawString("JUMP", 400, 100);
+		z.setColor(Color.BLUE);
+		z.drawString("Game Over", 400, 100);
+		z.setColor(Color.BLUE);
+		z.drawString("Press Enter To Restart", 250, 500);
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			currentState++;
+
+			if (currentState > END_STATE) {
+				currentState = MENU_STATE;
+			}
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			Blocky.rightkey = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			Blocky.leftkey = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			Blocky.jump();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			Blocky.rightkey = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			Blocky.leftkey = false;
+		}
+	}
 }
